95c717a45ae4409398e2d55f2fe1aa7d48ea5363
Roshit Jain <Roshit.Jain@synchronoss.com>
2015-09-23 03:02:04 +0530

    Able to extract particular category of messages

--- remote-command.sh
+++ remote-command.sh
@@ -1,3 +1,5 @@
+# Script to extract logs from any comm server  
 COMM_SERVER_IP=$1
 COMM_SERVER_NO=$2
-ssh -i Ample-Load-Testing-Keypair.pem ubuntu@10.68.2."$COMM_SERVER_IP" "grep \"heartbeat\" /var/log/host$COMM_SERVER_IP\"_comm\"$COMM_SERVER_NO/commserver.log" 
+SSH_KEY_PATH=$3
+ssh -i $SSH_KEY_PATH ubuntu@10.68.2."$COMM_SERVER_IP" "grep \"type=\\\"heartbeat\\\"\" /var/log/host$COMM_SERVER_IP\"_comm\"$COMM_SERVER_NO/commserver.log" 
